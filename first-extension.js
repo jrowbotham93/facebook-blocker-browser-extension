3a7b1e7582e18d4f1db78f5099e0392168ce7931
jrowbotham <james.rowbotham@finaps.nl>
2019-08-14 12:54:31 +0200

    first commit

---
+++ first-extension.js
@@ -0,0 +1,11 @@
+function facebookBlock(){
+  document.body.style.display = "none"
+  document.getElementById("mainContainer").style.display = "none";
+  document.getElementById("content_container").style.display = "none"
+};
+
+function facebookUnblock(){
+
+}
+
+// if you answer these questions correctly then I will unblock
